e13a3a10c8899bd6e3329c1446385e370f974e1d
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-09 00:34:07 -0700

    Added GetRelativePath extension to RemoteFileInfo which provides the usable Path (i.e. relative to the application's install directory) for all RemoteFileInfo actions (such as Get / Put etc)

--- WindowsPhone.Tools/CoreConExtensions.cs
+++ WindowsPhone.Tools/CoreConExtensions.cs
@@ -9,10 +9,18 @@ namespace WindowsPhone.Tools
 {
     public static class CoreConExtensions
     {
+        private const string RELATIVE_PATH_SEPARATOR = "IsolatedStore\\";
+        private static int RELATIVE_PATH_SEPARATOR_LENGTH   = RELATIVE_PATH_SEPARATOR.Length;
 
         public static string GetExtension(this RemoteFileInfo fileInfo)
         {
             return Path.GetExtension(fileInfo.Name);
+        }
+
+        public static string GetRelativePath(this RemoteFileInfo fileInfo)
+        {
+            string name = fileInfo.Name;
+            return name.Substring(name.IndexOf(RELATIVE_PATH_SEPARATOR) + RELATIVE_PATH_SEPARATOR_LENGTH);
         }
 
     }
