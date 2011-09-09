664d4430b7242e33a74b8ed761cd8723b91c6ba2
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-09 00:35:06 -0700

    Helper class which provides the GetLastDirectory function, i.e. for c:\a\b\c\file.ext returns 'c'

---
+++ WindowsPhone.Tools/PathHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WindowsPhone.Tools
+{
+    internal static class PathHelper
+    {
+        private static string DirectorySeparatorString = Path.DirectorySeparatorChar + "";
+
+        /// <summary>
+        /// Returns the last directory from a path
+        /// 
+        /// c:\a\b\c\file.txt returns "c"
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string GetLastDirectory(string path) {
+
+            // often paths get passed in as c:\temp instead of c:\temp\, check for this
+            // and add a separator. This is not performant, but functional, if you need
+            // a performant copy, remove this line :)
+            if (Directory.Exists(path) && !path.EndsWith(DirectorySeparatorString))
+                path += Path.DirectorySeparatorChar;
+
+            // get the directory from the path, remove the trailing \ and return the resulting filename
+            return Path.GetFileName(Path.GetDirectoryName(path).TrimEnd(Path.DirectorySeparatorChar));
+
+        }
+    }
+}
