6dbec0a274693f2496c7b9f12aa6e3fb0d6257a3
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-11 19:13:41 -0700

    Binding converter to convert a stream into a BitmapImage suitable for using as an Image source

---
+++ WindowsPhoneToolbox/StreamToBitmapImageConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace WindowsPhoneToolbox
+{
+    class StreamToBitmapImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            BitmapImage image = new BitmapImage();
+
+            try
+            {
+                if (value != null)
+                {
+                    image.BeginInit();
+                    image.StreamSource = value as Stream;
+                    image.EndInit();
+                }
+            }
+            catch {
+                image = FileTypeToIconConverter.imageApp;
+            }
+
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
