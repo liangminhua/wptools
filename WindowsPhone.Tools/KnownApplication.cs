453254b6b0173380d2a80d9c579f72385f95c565
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-11 19:13:07 -0700

    KnownApplication class to serialize more information about a known xap, beyond just the name

---
+++ WindowsPhone.Tools/KnownApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsPhone.Tools
+{
+    /// <summary>
+    /// This is mainly for persisting data about known applications and you shouldn't
+    /// generally need to interact with it directly
+    /// </summary>
+    [Serializable]
+    internal class KnownApplication
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+    }
+}
