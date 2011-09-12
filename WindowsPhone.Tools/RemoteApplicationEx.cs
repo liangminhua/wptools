a77e8f4b8a26492ad50dbf4c6ca71cd8b51ab440
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-11 19:16:04 -0700

    Added Icon property which returns a stream to the app's icon if it was persisted

--- WindowsPhone.Tools/RemoteApplicationEx.cs
+++ WindowsPhone.Tools/RemoteApplicationEx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.SmartDevice.Connectivity;
+using System.IO;
+using System.IO.IsolatedStorage;
 
 namespace WindowsPhone.Tools
 {
@@ -10,6 +12,19 @@ namespace WindowsPhone.Tools
     {
 
         public RemoteApplication RemoteApplication { get; private set; }
+
+        bool inited = false;
+        private KnownApplication _knownApplication;
+
+        private bool Init(Guid guid)
+        {
+            if (inited && _knownApplication == null)
+                return false;
+
+            inited = true;
+
+            return PersistedData.Current.KnownApplication.TryGetValue(guid, out _knownApplication);
+        }
 
         private string _name;
         public string Name
@@ -20,13 +35,54 @@ namespace WindowsPhone.Tools
                 {
                     Guid productId = RemoteApplication.ProductID;
 
-                    if (!PersistedData.Current.KnownApplication.TryGetValue(productId, out _name))
+                    if (Init(productId))
+                    {
+                        _name = _knownApplication.Name;
+                    } 
+                    else
                     {
                         _name = productId.ToString();
                     }
                 }
 
                 return _name;
+            }
+        }
+
+        public Stream Icon
+        {
+            get
+            {
+                try
+                {
+                    if (Init(RemoteApplication.ProductID))
+                    {
+                        string file = _knownApplication.Icon;
+
+                        if (file != null)
+                        {
+                            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForAssembly())
+                            {
+                                if (store.FileExists(file))
+                                {
+                                    using (IsolatedStorageFileStream sourceStream = store.OpenFile(file, FileMode.Open)) {
+                                        // copy the stream from IsolatedStorage to a generic MemoryStream
+                                        MemoryStream stream = new MemoryStream();
+
+                                        sourceStream.CopyTo(stream);
+                                        
+                                        stream.Seek(0, SeekOrigin.Begin);
+
+                                        return stream;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch { }
+
+                return null;
             }
         }
 
