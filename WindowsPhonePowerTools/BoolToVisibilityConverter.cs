82905361151eb2c2645290741198f66a4cba83ba
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-14 18:19:18 -0700

    Initial thoughts for a device connection indicator control, which acts similar to Zune's account information, indicating login status. Implementation is not complete.

---
+++ WindowsPhonePowerTools/BoolToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace WindowsPhonePowerTools
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool? source  = value as bool?;
+            bool reverse;
+
+            if (!bool.TryParse(parameter as string, out reverse))
+                reverse = false;
+
+            if ((source == true && !reverse) || (source == false && reverse))
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WindowsPhonePowerTools/DeviceConnectionIndicator.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace WindowsPhonePowerTools
+{
+    /// <summary>
+    /// Interaction logic for DeviceConnectionIndicator.xaml
+    /// </summary>
+    public partial class DeviceConnectionIndicator : UserControl, INotifyPropertyChanged
+    {
+
+        #region Dependency Properties
+
+        public SolidColorBrush DisconnectedColor
+        {
+            get { return (SolidColorBrush)GetValue(DisconnectedColorProperty); }
+            set 
+            { 
+                SetValue(DisconnectedColorProperty, value);
+
+                if (!Connected)
+                    StatusColor = value;
+            }
+        }
+
+        public static readonly DependencyProperty DisconnectedColorProperty =
+           DependencyProperty.Register("DisconnectedColorProperty", typeof(SolidColorBrush), typeof(DeviceConnectionIndicator), new UIPropertyMetadata(null));
+
+
+        public SolidColorBrush ConnectedColor
+        {
+            get { return (SolidColorBrush)GetValue(ConnectedColorProperty); }
+            set 
+            { 
+                SetValue(ConnectedColorProperty, value);
+
+                if (Connected)
+                    StatusColor = value;
+            }
+        }
+
+        public static readonly DependencyProperty ConnectedColorProperty =
+           DependencyProperty.Register("ConnectedColorProperty", typeof(SolidColorBrush), typeof(DeviceConnectionIndicator), new UIPropertyMetadata(null));
+
+        #endregion
+
+        #region Properties
+
+        private bool? _connected;
+        public bool Connected
+        {
+            get { return (_connected == true ? true : false); }
+            set
+            {
+                if (_connected != value)
+                {
+                    _connected = value;
+
+                    NotifyPropertyChanged("Connected");
+
+                    if (_connected == true)
+                    {
+                        StatusColor = ConnectedColor;
+                    }
+                    else
+                    {
+                        StatusColor = DisconnectedColor;
+                    }
+                }
+            }
+        }
+
+        private SolidColorBrush _statusColor;
+        public SolidColorBrush StatusColor
+        {
+            get { return _statusColor; }
+            private set
+            {
+                if (_statusColor != value)
+                {
+                    _statusColor = value;
+
+                    NotifyPropertyChanged("StatusColor");
+                }
+            }
+        }
+
+        #endregion
+
+        public DeviceConnectionIndicator()
+        {
+            InitializeComponent();
+        }
+
+        #region Events
+        
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Connected = false;
+        }
+
+        #endregion
+
+
+        # region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        #endregion
+
+    }
+}
