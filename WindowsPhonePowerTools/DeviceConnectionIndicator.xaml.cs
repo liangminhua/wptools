33b83a4e93ae87db33fc077c1e1557ee6e98d56d
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-15 09:10:17 -0700

    Set up binding of DeviceConnectionIndicator (DataContext should be the WindowsPhoneDevice)

--- WindowsPhonePowerTools/DeviceConnectionIndicator.xaml.cs
+++ WindowsPhonePowerTools/DeviceConnectionIndicator.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
+using WindowsPhone.Tools;
+using Microsoft.SmartDevice.Connectivity;
 
 namespace WindowsPhonePowerTools
 {
@@ -21,7 +23,49 @@ namespace WindowsPhonePowerTools
     public partial class DeviceConnectionIndicator : UserControl, INotifyPropertyChanged
     {
 
+        #region DataContext sink
+
+        private INotifyPropertyChanged _dataContext;
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (_dataContext != e.NewValue)
+            {
+                // deregister from the old control
+                if (_dataContext != null) 
+                {
+                    _dataContext.PropertyChanged -= _dataContext_PropertyChanged;
+                }
+
+                // remember the new DataContext and register for notifications
+                _dataContext = e.NewValue as INotifyPropertyChanged;
+                _dataContext.PropertyChanged += _dataContext_PropertyChanged;
+            }
+        }
+
+        private void _dataContext_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            WindowsPhoneDevice device = sender as WindowsPhoneDevice;
+
+            if (device != null)
+            {
+                if (e.PropertyName == "Connected")
+                    Connected = device.Connected;
+            }
+        }
+
+        #endregion
+
         #region Dependency Properties
+
+        public Device Device
+        {
+            get { return (Device)GetValue(DeviceProperty); }
+            set { SetValue(DeviceProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceProperty =
+           DependencyProperty.Register("Device", typeof(Device), typeof(DeviceConnectionIndicator), new UIPropertyMetadata(null));
 
         public SolidColorBrush DisconnectedColor
         {
@@ -36,8 +80,7 @@ namespace WindowsPhonePowerTools
         }
 
         public static readonly DependencyProperty DisconnectedColorProperty =
-           DependencyProperty.Register("DisconnectedColorProperty", typeof(SolidColorBrush), typeof(DeviceConnectionIndicator), new UIPropertyMetadata(null));
-
+           DependencyProperty.Register("DisconnectedColor", typeof(SolidColorBrush), typeof(DeviceConnectionIndicator), new UIPropertyMetadata(null));
 
         public SolidColorBrush ConnectedColor
         {
@@ -52,9 +95,11 @@ namespace WindowsPhonePowerTools
         }
 
         public static readonly DependencyProperty ConnectedColorProperty =
-           DependencyProperty.Register("ConnectedColorProperty", typeof(SolidColorBrush), typeof(DeviceConnectionIndicator), new UIPropertyMetadata(null));
+           DependencyProperty.Register("ConnectedColor", typeof(SolidColorBrush), typeof(DeviceConnectionIndicator), new UIPropertyMetadata(null));
 
         #endregion
+
+        
 
         #region Properties
 
@@ -102,8 +147,10 @@ namespace WindowsPhonePowerTools
         public DeviceConnectionIndicator()
         {
             InitializeComponent();
-        }
 
+            this.DataContextChanged += new DependencyPropertyChangedEventHandler(OnDataContextChanged);
+        }
+        
         #region Events
         
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
