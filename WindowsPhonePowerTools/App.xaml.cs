74969c215fc165eec6e276b38edae27808e3546f
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-20 18:58:52 -0700

    Fixed GUID binding

--- WindowsPhonePowerTools/App.xaml.cs
+++ WindowsPhonePowerTools/App.xaml.cs
@@ -43,7 +43,6 @@ namespace WindowsPhonePowerTools
                     {
                         needGenericMessage = true;
                     }
-
                 }
                 else
                 {
