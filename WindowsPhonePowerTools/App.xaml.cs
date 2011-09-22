fca69d20264e75856840caa900bab502b55b6a1c
orena <orena@oren10.redmond.corp.microsoft.com>
2011-09-23 08:12:31 +1000

    More human readable exceptions

--- WindowsPhonePowerTools/App.xaml.cs
+++ WindowsPhonePowerTools/App.xaml.cs
@@ -45,11 +45,24 @@ namespace WindowsPhonePowerTools
                     {
                         MessageBox.Show("Oops! Looks like your device is not Developer Unlocked.\n\nUnlock your phone using the \"Windows Phone Developer Registration\" tool and try again",
                             "Phone Developer Locked", MessageBoxButton.OK, MessageBoxImage.Warning);
-                    } else if (e.Exception.Message == "0x81030120") 
+                    } 
+                    else if (e.Exception.Message == "0x81030120") 
                     {
                         MessageBox.Show(
                             "I tried to install / update your xap but your phone rejected it because it uses capabilities that your developer account is not provisioned for (usually ID_CAP_INTEROPSERVICES).\n\nIf you believe that your account should be provisioned for these then please contact AppHub support and try unregistering and reregistering your phone.\n\nIf you believe that this is a bug in the tools, and your XAP should have installed correctly, feel free to log a bug (with a repro xap) at http://wptools.codeplex.com",
                             "Denied by Developer Unlock", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                    else if (e.Exception.Message == "0x81030121")
+                    {
+                        MessageBox.Show(
+                            "Unable to launch application that requires ID_CAP_INTEROPSERVICES on your developer account.\n\nIf you believe that this is a mistake, contact AppHub support.\n\nNote: This is a newly enforced restriction in Windows Phone Mango and will apply to previously installed developer Apps that used ID_CAP_INTEROPSERVICES, as well as to any newly installed apps.",
+                            "Developer Account Not Allowed to use ID_CAP_INTEROPSERVICES", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                    else if (e.Exception.Message == "0x81030119")
+                    {
+                        MessageBox.Show(
+                            "Unable to install xap - you've reached the maximum number of developer xaps that you can install. Uninstall a developer app and try again",
+                            "Too Many Developer Apps", MessageBoxButton.OK, MessageBoxImage.Warning);
                     }
                     else
                     {
