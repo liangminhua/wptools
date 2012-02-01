d1778322512009f355b932858b06ff9013c49963
nachmore <nachmore@localhost>
2012-01-31 19:00:31 -0800

    Simple console exceptions

---
+++ WindowsPhonePowerTools.Console/Exceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsPhonePowerTools.Console
+{
+    /// <summary>
+    /// Something fatal has happened, let's exit with a nice message
+    /// </summary>
+    class ConsoleMessageException : Exception
+    {
+        public ConsoleMessageException(string msg) : base(msg) { }
+    }
+}
